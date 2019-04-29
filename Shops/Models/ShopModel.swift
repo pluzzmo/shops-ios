252e341c81c84c833097cf084e1862946ca7db5f
Ramzi BOUKERI <ramzi.boukeri@ext.mpsa.com>
2019-04-29 18:18:02 +0200

    Create ShopModel.swift
    It wil represent our shop model with all attributes

---
+++ Shops/Models/ShopModel.swift
@@ -0,0 +1,29 @@
+//
+//  ShopModel.swift
+//  Shops
+//
+//  Created by Ramzi BOUKERI on 29/04/2019.
+//  Copyright © 2019 Ramzi BOUKERI. All rights reserved.
+//
+
+import Foundation
+
+struct ShopModel {
+    
+    var identifier: Double
+    var shopId: Double
+    var latitude: String
+    var longitude: String
+    var distance: String
+    var name: String
+    var chain: String
+    var address: String
+    var zipcode: String
+    var city: String
+    var categoryId: Double
+    var categoryName: String
+    var logoUrl: String
+    var cover: String
+    var maxOffer: String
+    var currency: String
+}
